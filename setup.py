70937389647ec2e0df4390f53a849bc0e280452d
Arunvel Sriram <arunvelsriram@gmail.com>
2020-11-07 22:25:03 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+install_requires = [
+    'apache-airflow>=1.10.4'
+]
+
+setup(
+    name='dag-timeline',
+    version='0.1.0',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    url='',
+    license='',
+    author='Arunvel Sriram',
+    author_email='arunvelsriram@gmail.com',
+    description='Airflow plugin to show DAG schedules in a timeline',
+    install_requires=install_requires,
+    entry_points={
+        'airflow.plugins': [
+            'DAGTimelinePlugin = dag_timeline.dag_timeline:DAGTimelinePlugin'
+        ]
+    }
+)
