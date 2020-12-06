63558d75c8815e0153f217cf710b97e7aabaf0d6
Arunvel Sriram <arunvelsriram@gmail.com>
2020-12-06 16:23:26 +0530

    Set timezone to UTC in UI and add min and max values for x-axis

--- dag_timeline/dag_timeline_view.py
+++ dag_timeline/dag_timeline_view.py
@@ -5,7 +5,7 @@ from airflow import settings
 from flask_appbuilder import BaseView, expose
 
 from dag_timeline import logger
-from dag_timeline.timeline import get_dag_schedules
+from dag_timeline.timeline import get_dag_schedules, timestamp_ms
 
 
 class DAGTimelineView(BaseView):
@@ -14,11 +14,14 @@ class DAGTimelineView(BaseView):
     @expose('/')
     def list(self):
         tz = settings.TIMEZONE
-        from_dt = pendulum.today(tz=tz)
-        to_dt = pendulum.tomorrow(tz=tz)
-        logger.info(f"Getting DAG schedules - from: {from_dt} and to: {to_dt}")
-        dag_schedules = get_dag_schedules(from_dt, to_dt)
-        return self.render_template('dag_timeline/index.html', dag_schedules=json.dumps(dag_schedules))
+        from_dttm = pendulum.today(tz=tz)
+        to_dttm = pendulum.tomorrow(tz=tz)
+        logger.info(f"Getting DAG schedules - from: {from_dttm} and to: {to_dttm}")
+        dag_schedules = get_dag_schedules(from_dttm, to_dttm)
+        return self.render_template('dag_timeline/index.html',
+                                    from_dttm=timestamp_ms(from_dttm),
+                                    to_dttm=timestamp_ms(to_dttm),
+                                    dag_schedules=json.dumps(dag_schedules))
 
 
 dag_timeline_view = {
