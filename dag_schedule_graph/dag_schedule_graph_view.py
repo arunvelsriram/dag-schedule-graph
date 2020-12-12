eb5dc4cad2859dee72c3877ede07d5c123667bbe
Arunvel Sriram <arunvelsriram@gmail.com>
2020-12-12 16:21:38 +0530

    Fix timezone to UTC in backend (for initial relase only supporting UTC)

--- dag_schedule_graph/dag_schedule_graph_view.py
+++ dag_schedule_graph/dag_schedule_graph_view.py
@@ -1,7 +1,6 @@
 import json
 
 import pendulum
-from airflow import settings
 from flask_appbuilder import BaseView, expose
 
 from dag_schedule_graph import logger
@@ -13,9 +12,9 @@ class DAGScheduleGraphView(BaseView):
 
     @expose('/')
     def list(self):
-        tz = settings.TIMEZONE
-        from_dttm = pendulum.today(tz=tz)
-        to_dttm = pendulum.tomorrow(tz=tz)
+        utc = pendulum.timezone('UTC')
+        from_dttm = pendulum.today(tz=utc)
+        to_dttm = pendulum.tomorrow(tz=utc)
         logger.info(f'Getting DAG schedules - from: {from_dttm} and to: {to_dttm}')
         dag_schedules = get_dag_schedules(from_dttm, to_dttm)
         return self.render_template('dag_schedule_graph/index.html',
