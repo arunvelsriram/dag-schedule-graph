ca4119ded09d41923784296c07a4db2b3f967668
Arunvel Sriram <arunvelsriram@gmail.com>
2020-12-12 13:31:28 +0530

    Add more test dags

--- airflow/dags/test_dag.py
+++ airflow/dags/test_dag.py
@@ -10,8 +10,8 @@ args = {
     'start_date': days_ago(2),
 }
 
-dag = DAG(
-    dag_id='test_dag',
+dag1 = DAG(
+    dag_id='test_dag1',
     default_args=args,
     schedule_interval='0 */2 * * *',
     tags=['example']
@@ -19,13 +19,13 @@ dag = DAG(
 
 run_this_last = DummyOperator(
     task_id='run_this_last',
-    dag=dag,
+    dag=dag1,
 )
 
 run_this = BashOperator(
     task_id='run_after_loop',
     bash_command='echo 1',
-    dag=dag,
+    dag=dag1,
 )
 
 run_this >> run_this_last
@@ -34,13 +34,55 @@ for i in range(3):
     task = BashOperator(
         task_id='runme_' + str(i),
         bash_command='echo "{{ task_instance_key_str }}" && sleep 1',
-        dag=dag,
+        dag=dag1,
     )
     task >> run_this
 
 also_run_this = BashOperator(
     task_id='also_run_this',
     bash_command='echo "run_id={{ run_id }} | dag_run={{ dag_run }}"',
-    dag=dag,
+    dag=dag1,
 )
 also_run_this >> run_this_last
+
+dag2 = DAG(
+    dag_id='test_dag2',
+    default_args=args,
+    schedule_interval='0 */1 * * *',
+    tags=['example']
+)
+
+t1 = BashOperator(
+    task_id='test_dag2_hi',
+    bash_command="echo Hi",
+    dag=dag2
+)
+
+t2 = BashOperator(
+    task_id='test_dag2_bye',
+    bash_command="echo Bye",
+    dag=dag2
+)
+
+t1 >> t2
+
+dag3 = DAG(
+    dag_id='test_dag3',
+    default_args=args,
+    schedule_interval='0 */1 * * *',
+    tags=['example']
+)
+
+t3 = BashOperator(
+    task_id='test_dag3_hi',
+    bash_command="echo Hi",
+    dag=dag2
+)
+
+t4 = BashOperator(
+    task_id='test_dag3_bye',
+    bash_command="echo Bye",
+    dag=dag2
+)
+
+t3 >> t4
